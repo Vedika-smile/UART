56e48b255cfc721f333659e0918a8aa9088dcd86
Vedika-smile <vedikamalpani38@gmail.com>
2026-02-02 15:08:32 +0530

    Add files via upload

--- bauderate_gen.v
+++ bauderate_gen.v
@@ -1,22 +1,64 @@
-module baudrate_gen (clk, tx_enb, rx_enb);
- input clk;
- output tx_enb, rx_enb;
- reg [12:0] tx_counter;
- reg [9:0] rx_counter;
+module baudrate_gen(input clock,reset,output reg enb_tx,enb_rx);
+    parameter clk_freq = 100000000; // SYSTEM CLOCK FREQUENCY
+    parameter baud_rate = 9600; //REQUIRED BAUD RATE
+    reg [15:0] counter_tx; //REGISTER FOR CREATING THE SENDER CLOCK
+    reg [15:0] counter_rx; // REGISTER FOR CREATING THE RECIEVER CLOCK
 
- always @(posedge clk)
-  begin 
-   if (tx_counter == 5208) tx_counter = 0;
-   else tx_counter = tx_counter + 1;
-  end
-
- always @(posedge clk)
-  begin
-   if(rx_counter == 325) rx_counter = 0;
-   else rx_counter = rx_counter +1 ;
-  end
-
- assign tx_enb = (tx_counter == 0) ? 1 : 0;
- assign rx_enb = (rx_counter == 0) ? 1 : 0;
- 
+    parameter divisor_tx = clk_freq/baud_rate; //PRESCALAR OF SENDER
+    parameter divisor_rx = clk_freq/(16 *baud_rate);    //PRESCALAR OF RECIEVER
+        
+    //SENDER CLOCK GENERATION LOGIC
+    
+    always@(posedge clock)
+        begin
+        
+            if(reset)
+                begin
+                    counter_tx <= 0;
+                    enb_tx = 0;
+                    enb_rx = 0;
+                 end
+       //FOR 10,416 CLOCK CYCLES OF SYSTEM CLOCK 1 CLOCK CYCLE IS GENERATED
+                 
+            else if(counter_tx == divisor_tx - 1)
+                begin
+                    enb_tx <= 1;
+                    counter_tx <= 0;
+                    
+                 end
+              else
+                begin
+                   counter_tx <= counter_tx + 1'b1;
+                   enb_tx = 0;
+                    
+               end
+     end
+     
+     
+    //LOGIC FOR RECIEVER CLOCK
+      
+        always@(posedge clock)
+            begin
+            
+                if(reset)
+                    begin
+                        counter_rx <= 0;
+                     end
+   
+   //FOR GENERATING SENDER CLOCK
+                     
+                else if(counter_rx == divisor_rx - 1)
+                    begin
+                        counter_rx <= 0;
+                        enb_rx = 1;
+                        
+                     end
+                  else
+                    begin
+                        counter_rx <= counter_rx + 1;
+                        enb_rx = 0;
+                        
+                   end
+         end 
+    
 endmodule
